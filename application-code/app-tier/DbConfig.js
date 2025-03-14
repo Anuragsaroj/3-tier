1e9a37617646ca8f3f23986113fb61a9a2c61216
Anuragsaroj <anuragsaroj0008@gmail.com>
2025-03-14 19:00:17 +0530

    first

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database.cxuk0g0a2wmz.us-west-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Anurag12345',
+    DB_DATABASE : 'webappdb'
+});
